4f192125607abbf65c5ecab1f7a56bb720fea3ef
vikram babu kuruguntla <vikram.babu.kuruguntla@oracle.com>
2020-07-16 22:06:42 +0530

    changed the font size

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -19,7 +19,7 @@ import {createNewTable} from "../action/ScoreAction";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
+        flexGrow: 1
     },
     menuButton: {
         marginRight: theme.spacing(2),
