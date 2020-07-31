7ffea71978755e56cbf5007314d0c171d3c8b409
vikram babu kuruguntla <vikram.babu.kuruguntla@oracle.com>
2020-07-31 15:31:04 +0530

    player score by date

--- src/components/players/PlayerScore.jsx
+++ src/components/players/PlayerScore.jsx
@@ -21,7 +21,15 @@ export class NewTablePage extends React.Component {
                 <SimpleAppBar showAllTables={() => {this.props.history.push("/all-tables")}} onCreateTableFn={() => this.props.history.push("/create-table")} showPlayerScore={()=>this.props.history.push("/player-score")}/>
                 {
                     !this.props.playerScores || this.props.playerScores.size === 0 ? '' :
-                        <SimpleTable names={Object.keys(this.props.playerScores.toJS())} values={Object.keys(this.props.playerScores.toJS()).map(k => this.props.playerScores.toJS()[k])}/>
+                        Object.keys(this.props.playerScores.toJS()).map(date => {
+                            const namesMap = this.props.playerScores.toJS();
+                            return (
+                                <div>
+                                    <span>{date}</span>
+                                    <SimpleTable names={Object.keys(namesMap[date])} values={Object.keys(namesMap[date]).map(k => namesMap[date][k])}/>
+                                </div>
+                            )
+                        })
                 }
             </div>
         )
